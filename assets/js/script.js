c8d78246e4b1b5d9aabc2a49b04b530af8f5dd53
tmstafford <tatumstafford1@gmail.com>
2020-11-13 12:25:00 -0600

    sentence and paragraph count

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 let textArea = document.querySelector("#textarea");
 let wordCount = document.querySelector("#wordCount");
 let characterCount = document.querySelector("#characterCount");
+let sentenceCount = document.querySelector("#sentenceCount");
+let paragraphCount = document.querySelector("#paragraphCount");
 
 let countWords = function() {
     // get number of characters
@@ -9,11 +11,25 @@ let countWords = function() {
     // get word count 
     // look for word boundaries and valid word characters- more effective
     let words = textArea.value.match(/\b[-?(\w+)?]+\b/gi); 
-    console.log(words);
+    // console.log(words);
     if (words) {
         wordCount.textContent = words.length;
     } else {
         wordCount.textContent = "0";
+    }
+    // sentence count with .!? to seperate sentences
+    // paragraph count 
+    if (words) {
+        let sentences = textArea.value.split(/[.|!|?]+/g);
+        // console.log(sentences);
+        sentenceCount.textContent = sentences.length - 1;
+        
+        let paragraphs = textArea.value.replace(/\n$/gm, '').split(/\n/);
+        paragraphCount.textContent = paragraphs.length;
+       // console.log(paragraphs);
+    } else {
+        sentenceCount.textContent = "0";
+        paragraphCount.textContent = "0";
     }
 };
 
