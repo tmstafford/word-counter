5f083509fa059a42c0d2a30d18470fc342493cc8
tmstafford <tatumstafford1@gmail.com>
2020-11-13 12:13:08 -0600

    edited to look for word boundaries

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,20 @@
-let textareaEl = document.querySelector("#textarea");
-let wordCountEl = document.querySelector("#wordCount");
-let characterCountEl = document.querySelector("#characterCount");
+let textArea = document.querySelector("#textarea");
+let wordCount = document.querySelector("#wordCount");
+let characterCount = document.querySelector("#characterCount");
 
 let countWords = function() {
     // get number of characters
-    let characters = textareaEl.value.length;
-    characterCountEl.textContent = characters;
+    let characters = textArea.value.length;
+    characterCount.textContent = characters;
     // get word count 
-    let text = textareaEl.value.split(' ');
-    let wordCount = 0;
-    // loop through array checking for empty space
-    for (i = 0; i < text.length; i++) {
-        if (text[i] !== ' ') {
-            wordCount++;
-        }
+    // look for word boundaries and valid word characters- more effective
+    let words = textArea.value.match(/\b[-?(\w+)?]+\b/gi); 
+    console.log(words);
+    if (words) {
+        wordCount.textContent = words.length;
+    } else {
+        wordCount.textContent = "0";
     }
-    // display word count 
-    wordCountEl.textContent = wordCount;
 };
 
-textareaEl.addEventListener("keyup", countWords);
+textArea.addEventListener("keyup", countWords);
